71a226bdd06ea1ca843f3ad4b9f65917d2e60fe5
mrnobody2002 <filipemelo032@gmail.com>
2020-01-29 23:46:45 -0300

    responsive screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,40 @@
 import React from 'react'
-import {Text, StyleSheet, Image, TextInput, ImageBackground, TouchableOpacity, View, SafeAreaView} from 'react-native'
+import {Text, StyleSheet, Image, TextInput, ScrollView, TouchableOpacity, View, SafeAreaView} from 'react-native'
 import {MaterialIcons} from '@expo/vector-icons';
-
+import {heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen'
 import illustration from './assets/SearchIllustration.png'
 import logo from './assets/logo.png'
 export default function Home(){
     return (
-        <>
-      
-            <SafeAreaView style={styles.view}>
+    <>
+        <SafeAreaView style={styles.view}>
+            <ScrollView>
+                <View>
                     <View style={styles.header}>
                         <View style={styles.headerTitle}>
                             <Text style={styles.title}>Easy N' Taste</Text>
                             <Text style={styles.info}>Encotre as melhores receitas pesquisando pelos ingredientes que você tem em casa ;)</Text>
-                        </View>        
+                        </View>
                         <Image source={logo} style={styles.headerLogo}/>
                     </View>
-                
-                
-                <View style={styles.formGroup}>
-                    <TextInput style={styles.input} 
-                        autoCapitalize="words" 
-                        autoCorrect={false}
-                        placeholder="Batata, carne, cebola..."
-                        autoFocus={true}
-                        keyboardType="web-search"
-                    />
-                    <TouchableOpacity style={styles.btn}>
-                        <MaterialIcons size={45} name='search' color="#eb0"/>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.suggestionsBlock}>
-                    <Text style={styles.subTitle}>Sugestões...</Text>
-                    <View style={styles.cardSuggestions}>
-                        <Image source={illustration} style={styles.cardIllustration}/>
-                        <Text style={styles.cardText}>Nenhuma sugestão disponível. Realize sua primeira pesquisa ;)</Text>
+
+                    <View style={styles.formGroup}>
+                        <TextInput style={styles.input} autoCapitalize="words" autoCorrect={false} placeholder="Batata, carne, cebola..." autoFocus={true} keyboardType="web-search" />
+                        <TouchableOpacity style={styles.btn}>
+                            <MaterialIcons size={45} name='search' color="#eb0" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.suggestionsBlock}>
+                        <Text style={styles.subTitle}>Sugestões...</Text>
+                        <View style={styles.cardSuggestions}>
+                            <Image source={illustration} style={styles.cardIllustration}/>
+                            <Text style={styles.cardText}>Nenhuma sugestão disponível. Realize sua primeira pesquisa ;)</Text>
+                        </View>
                     </View>
                 </View>
-                
-            </SafeAreaView>
-        </>
+            </ScrollView>
+        </SafeAreaView>
+    </>
     )
 }
 
@@ -53,9 +48,7 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         backgroundColor: '#171717',
         padding: 30,
-        paddingTop:100,
-        alignContent:"space-between",
-        justifyContent:"space-between",
+        paddingTop:100
     },
     headerTitle:{
         flex:1,
@@ -64,26 +57,26 @@ const styles = StyleSheet.create({
         marginRight: 10
     },
     headerLogo:{
-        width: 150,
-        height: 150,
+        flex:1,
+        width: wp('20%'),
+        height: hp('20%'),
         marginLeft: 10,
     },
     title:{
         color:'#ffc800',
         fontWeight: '700',
-        fontSize:25
+        fontSize:hp('3.2%')
     },
     info:{
         textAlign:"justify",
-        fontSize: 16,
+        fontSize: hp('2%'),
         fontStyle:"italic",
         color:'#fff',
     },
     subTitle:{
         color:'#fff',
         fontWeight: '700',
-        top: 40,
-        fontSize:25
+        fontSize:hp('3.2%')
     },
     img:{
         width: 200,
@@ -132,7 +125,7 @@ const styles = StyleSheet.create({
         padding: 20,
         alignItems:"center",
         flexDirection:"row",
-        top:50,
+        top:10,
         shadowColor:'#000',
         borderTopLeftRadius:25,
         borderBottomRightRadius:25,
@@ -145,15 +138,15 @@ const styles = StyleSheet.create({
         elevation:6,
     },
     cardIllustration:{
-        width: 170,
-        height: 170,
+        width: wp('15%'),
+        height: hp('15%'),
         flex:1,
     },
     cardText:{
         flex:1,
         padding: 7,
         textAlign:"justify",
-        fontSize: 16,
+        fontSize: hp('2%'),
         fontStyle:"italic",
         color:'#2e2e2e',
     }
