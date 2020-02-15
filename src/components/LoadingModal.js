11628de87e1b64c5badeadc3da51fce79725ffaa
mrnobody2002 <filipemelo032@gmail.com>
2020-02-15 16:12:16 -0300

    Stying Modal screen

--- src/components/LoadingModal.js
+++ src/components/LoadingModal.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {View, ActivityIndicator, Modal} from 'react-native'
-
+import {View, ActivityIndicator, Modal, StyleSheet} from 'react-native'
 
 export default function LoadingModal({visible}){
     return(
-        <View  style={{marginTop: 22}}>
+        <View  style={styles.root}>
             <Modal
                 transparent={true}
+                style={styles.modal}
                 visible={visible}
-                animationType="slide"
+                animationType="fade"
             >
                 <View style={{marginTop: 22, padding:50}}>
                     <ActivityIndicator size="large" color="#0000ff" />
@@ -17,3 +17,12 @@ export default function LoadingModal({visible}){
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    root:{
+        marginTop:hp('50%'),
+    },
+    modal:{
+        backgroundColor:'#000000'
+    }
+})
